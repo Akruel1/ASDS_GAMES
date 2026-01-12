2627093bfca85ff5ea436272e7b0c07b51ef0bed
Akruel1 <tecca5864@gmail.com>
2026-01-12 13:12:20 +0200

    Update: Fix button clickability, remove page rotation, add Twitch API integration, update year to 2026

--- commit_to_games.bat
+++ commit_to_games.bat
@@ -3,6 +3,6 @@ chcp 65001 >nul
 git remote set-url origin https://github.com/Akruel1/ASDS_GAMES.git
 git add .
 git commit -m "Update: Fix button clickability, remove page rotation, add Twitch API integration, update year to 2026"
+git pull origin main --allow-unrelated-histories --no-edit
 git push origin main
 pause
-
